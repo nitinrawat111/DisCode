0c7a968ce2e72412d0908ecfb5a29575a428f6fd
nitinrawat111 <rawat.nitin111@gmail.com>
2026-03-02 20:57:50 +0530

    fixed ApiError and ApiResponse to have optional fields

--- packages/backend/src/utils/ApiError.ts
+++ packages/backend/src/utils/ApiError.ts
@@ -1,13 +1,19 @@
-// Represents a custom thrown error for the API
+/**
+ * Represents a custom thrown error.
+ * Will be parsed automatically the application's global error handler.
+ *  - this does NOT represent the actual response sent to the client. 
+ *  - all responses should be wrapped in the ApiResponse format, 
+ *  - and this error will be parsed to fit that format by the global error handler.
+ */
 export class ApiError<ErrorType = unknown> extends Error {
   statusCode: number;
   message: string;
-  errors: ErrorType;
+  errors?: ErrorType;
 
   constructor(
     statusCode: number = 500,
     message: string = "Internal Server Error",
-    errors: ErrorType = undefined,
+    errors?: ErrorType,
   ) {
     super();
     this.name = "ApiError"; // To specifically indicate an ApiError (not general node.js Error), when logging
--- packages/backend/src/utils/ApiResponse.ts
+++ packages/backend/src/utils/ApiResponse.ts
@@ -1,15 +1,18 @@
-// Represents the API response object
+/**
+ * Represents data returned by the API.
+ * All responses from the API should be wrapped in this format.
+ */
 export class ApiResponse<DataType = unknown, ErrorType = unknown> {
   success: boolean;
   message: string;
-  data: DataType;
-  errors: ErrorType;
+  data?: DataType;
+  errors?: ErrorType;
 
   constructor(
     statusCode: number,
     message: string = "",
-    data: DataType = undefined,
-    errors: ErrorType = undefined,
+    data?: DataType,
+    errors?: ErrorType,
   ) {
     this.success = statusCode < 400;
     this.message = message;
