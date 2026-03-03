9a4e9e2ddd65e86adbb069947d3eca44fa617889
nitinrawat111 <rawat.nitin111@gmail.com>
2026-03-03 21:53:36 +0530

    add remaing services

---
+++ temp/problems-service/src/dtos/create-problem.dto.ts
@@ -0,0 +1,16 @@
+import zod from "zod";
+import {
+  titleDto,
+  markdownKeyDto,
+  testKeysDto,
+  difficultyDto,
+  tagsDto,
+} from "./problem.dto";
+
+export const createProblemDto = zod.object({
+  title: titleDto,
+  markdownKey: markdownKeyDto,
+  testKeys: testKeysDto,
+  difficulty: difficultyDto,
+  tags: tagsDto,
+});
