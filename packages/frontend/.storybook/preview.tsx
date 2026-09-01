96584fd5e62487c2f26b8613e4326cabeae4288a
nitinrawat111 <rawat.nitin111@gmail.com>
2026-09-01 19:54:41 +0530

    chore: add storybook

---
+++ packages/frontend/.storybook/preview.tsx
@@ -0,0 +1,31 @@
+import type { Preview } from "@storybook/react-vite";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+const preview: Preview = {
+  decorators: [
+    (Story) => (
+      <QueryClientProvider client={queryClient}>
+        <Story />
+      </QueryClientProvider>
+    ),
+  ],
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+
+    a11y: {
+      // 'todo' - show a11y violations in the test UI only
+      // 'error' - fail CI on a11y violations
+      // 'off' - skip a11y checks entirely
+      test: "todo",
+    },
+  },
+};
+
+export default preview;
--- packages/frontend/eslint.config.mjs
+++ packages/frontend/eslint.config.mjs
@@ -1,3 +1,4 @@
+import { configs } from "eslint-plugin-storybook";
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
@@ -48,4 +49,5 @@ export default defineConfig([
     },
   },
   globalIgnores(["node_modules", "dist", "logs"]),
+  ...configs["flat/recommended"],
 ]);
